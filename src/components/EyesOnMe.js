fed81c2387e630d580160b01dd54213b09da883a
Angela <balderasangela@gmail.com>
2022-12-23 15:43:48 -0500

    COMPLETED ASSIGNMENTS

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,23 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+function EyesOnMe() {
+
+    function haveAttention() {
+        console.log("Good!")
+    }
+
+    function wantAttention() {
+        console.log("Hey! Eyes on me!")
+    }
+
+  return (
+    <div>
+        <button onFocus={haveAttention} onBlur={wantAttention}>
+            Eyes on me
+        </button>
+    </div>
+  )
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,17 @@
 // Code Keypad Component Here
+import React from 'react'
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad() {
+    
+   const need = () => {
+console.log('Entering password...')
+   };
+   
+  return (
+    <div>
+        <input type="password" onChange={need}/>
+    </div>
+  )
 }
 
-export default Keypad;
+export default Keypad
